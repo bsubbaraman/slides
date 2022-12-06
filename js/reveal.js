a57f2c2c231db15a53367a39ec6ebaeeb17cc579
bsubbaraman <bms12014@mymail.pomona.edu>
2022-12-06 11:58:22 -0800

    more slides

--- js/reveal.js
+++ js/reveal.js
@@ -2592,6 +2592,33 @@ export default function( revealElement, options ) {
 
 	}
 
+/*
+	* Allow video to be inserted in fragment loop
+	*/
+
+	export default function loadVideoFragment() {
+
+		function playCurrentFragment() {
+		[].slice.call( document.querySelectorAll('.fragment') ).forEach(function(fragment) {
+
+			if(fragment.tagName == "VIDEO"){
+			var video = fragment;
+			if(video.classList.contains('current-fragment')) {
+				video.play();
+			}
+			else {
+				//video.pause();
+			}
+			}
+
+		});
+		}
+
+		Reveal.addEventListener( 'fragmentshown', playCurrentFragment );
+		Reveal.addEventListener( 'fragmenthidden', playCurrentFragment );
+		}
+		///
+
 
 	// --------------------------------------------------------------------//
 	// ------------------------------- API --------------------------------//
